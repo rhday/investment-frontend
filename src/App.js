c3de836b3194a104b4f50aef5e2d729448da1a9f
rhday <54642787+rhday@users.noreply.github.com>
2021-03-26 16:32:48 +0000

    installed react and redux

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import React from 'react';
 
 class App extends React.Component {
 
-  componentDidMount(){
-    fetch('http://localhost:3000/api/v1/accounts/1')
-    .then(res => res.json())
-    .then(data => console.log(data))
-  }
   render(){
     return(
       <div className="Investment App">
