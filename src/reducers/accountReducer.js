eeeafe7117114b716d9ca059104e2fdecb887010
rhday <54642787+rhday@users.noreply.github.com>
2021-03-29 12:35:29 +0100

    set up file tree in src and started 'accountReducer.js'

---
+++ src/reducers/accountReducer.js
@@ -0,0 +1,6 @@
+//pass in default state as an empty object with an empty array of 'accounts' inside and the action.
+export default function accountReducer(state = {accounts: []}){
+
+    return state
+
+}
