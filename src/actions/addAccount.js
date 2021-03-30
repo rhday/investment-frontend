832521fe14c4be9e018f647659569bac24191625
rhday <54642787+rhday@users.noreply.github.com>
2021-03-30 15:36:23 +0100

    built out AccountInput form

---
+++ src/actions/addAccount.js
@@ -0,0 +1,6 @@
+export const addAccount = (data) => {
+
+    return(dispatch) => {
+        
+    }
+}
--- src/components/AccountInput.js
+++ src/components/AccountInput.js
@@ -3,10 +3,32 @@ import React from 'react';
 
 class AccountInput extends React.Component {
 
+    state = {
+        name: '',
+        balance: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        
+    }
+
     render(){
         return(
             <div>
-                AccountInput
+                <form onSubmit={this.handleSubmit}>
+                    <label>Investment Account Name: </label>
+                    <input type="text" placeholder="Name" value={this.state.name} name="name" onChange={this.handleChange}/><br/>
+                    <label>Investment Account Balance: </label>
+                    <input type="text" placeholder="Balance" value={this.state.balance} name="balance" onChange={this.handleChange}/><br/>
+                    <input type="submit" />
+                </form>
             </div>
         );
     };
