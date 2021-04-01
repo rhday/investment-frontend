efd751b23cc5a195ec91d4dad4e0d362c0cbde14
rhday <54642787+rhday@users.noreply.github.com>
2021-04-01 13:59:33 +0100

    built out more Transaction rendering functionality

--- src/containers/TransactionsContainer.js
+++ src/containers/TransactionsContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TransactionInput from '../components/TransactionInput';
+import Transactions from '../components/Transactions';
 
 class TransactionsContainer extends React.Component {
 
@@ -7,6 +8,7 @@ class TransactionsContainer extends React.Component {
         return(
             <div>
                 <TransactionInput />
+                <Transactions  transactions={this.props.account && this.props.account.transactions}/>
             </div>
         )
     }
