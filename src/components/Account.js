bfe72f29a028db172ffb5ad71ff20843ccbca298
rhday <54642787+rhday@users.noreply.github.com>
2021-04-01 14:34:05 +0100

    built out TransactionInput form

--- src/components/Account.js
+++ src/components/Account.js
@@ -4,7 +4,9 @@ import {Redirect} from 'react-router-dom';
 
 const Account = (props) => {
 
-    let account = props.accounts[props.match.params.id - 1]
+    //let account = props.accounts[props.match.params.id - 1]
+
+    let account = props.accounts.filter(account => account.id == props.match.params.id)[0]
 
     return(
         <div>
