0e4147ac8776561fe83d5c731897bf5e80c7d1fb
rhday <54642787+rhday@users.noreply.github.com>
2021-04-06 15:41:18 +0100

    added bootstrap, some styling and set up Navbar

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import {deleteTransaction} from '../actions/deleteTransaction'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Button} from 'react-bootstrap'
+
 
 const Transactions = (props) => {
 
@@ -12,7 +15,7 @@ const Transactions = (props) => {
     return(
         <div>
           {props.transactions && props.transactions.map(transaction => 
-            <li key={transaction.id}>{transaction.kind} - {transaction.amount} - {transaction.asset}  <button onClick={() => handleDelete(transaction)}>Delete</button></li>
+            <li key={transaction.id}>{transaction.kind} - {transaction.amount} - {transaction.asset}  <Button variant="primary" onClick={() => handleDelete(transaction)}>Delete</Button></li>
             )}
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import accountReducer from './reducers/accountReducer';
 import {BrowserRouter as Router} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App';
 
