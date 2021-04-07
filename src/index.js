6f54a6c152560e64025058a6de1dbc9df289524d
rhday <54642787+rhday@users.noreply.github.com>
2021-04-07 15:52:35 +0100

    continued to build out login functionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import accountReducer from './reducers/accountReducer';
 import {BrowserRouter as Router} from 'react-router-dom';
+import usersReducer from './reducers/usersReducer';
+import currentUserReducer from './reducers/currentUserReducer';
+
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 import App from './App';
+
+const reducer = combineReducers({
+    users: usersReducer,
+    currentUserReducer
+})
 
 const composeEnhancers = (window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION__()) || compose;
 
